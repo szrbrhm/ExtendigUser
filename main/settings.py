94665b9af2844b74054ae4653e6f44afd12f3c3e
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-10-23 14:58:51 +0200

    add

--- main/settings.py
+++ main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os 
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+AUTH_USER_MODEL = 'users.User'
--- users/views.py
+++ users/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render, redirect, HttpResponse
 
 from django.contrib import messages
 
+from .forms import UserForm
+
 # Create your views here.
 
 def home(request):
     return render(request, 'users/home.html')
+
+def register(request):
+    form = UserForm()
+    
+    if request.method == 'POST':
+        pass
