5e71a1f32dc4b637feb4b3b067a606fdead4b145
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-10-23 16:06:27 +0200

    add

--- users/views.py
+++ users/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib import messages
 
 from .forms import UserForm
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
@@ -11,6 +14,37 @@ def home(request):
 
 def register(request):
     form = UserForm()
-    
+
+# Bu kod kullanıcıyı kaydedip login yapıp ana sayfaya yönlendiriyor..
     if request.method == 'POST':
-        pass
+        form = UserForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            return redirect('home')
+    
+    context = {
+        'form_user' : form
+    }
+    
+    return render(request, 'users/register.html', context)
+
+def user_logout(request):
+    messages.success(request, "You Logout!")
+    logout(request)
+    return redirect('home')
+
+def user_login(request):
+
+    form = AuthenticationForm(request, data=request.POST)
+
+    if form.is_valid():
+        user = form.get_user()
+        if user:
+            messages.success(request, "Login successfull")
+            login(request, user)
+            return redirect('home')
+    return render(request, 'users/user_login.html', {"form": form})
